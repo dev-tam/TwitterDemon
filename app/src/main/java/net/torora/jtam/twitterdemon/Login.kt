fc3510eae2dc7c1571163d642d3c5d6ed0b4c900
dev-tam <tamjid.mdh@gmail.com>
2017-12-16 08:34:56 +0600

    avatar image upload

--- app/src/main/java/net/torora/jtam/twitterdemon/Login.kt
+++ app/src/main/java/net/torora/jtam/twitterdemon/Login.kt
@@ -1,8 +1,16 @@
 package net.torora.jtam.twitterdemon
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -12,8 +20,53 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         ivImagePerson.setOnClickListener(View.OnClickListener {
-            //TODO: select image from phone
+            checkPermission()
         })
+    }
+
+    val READIMAGE:Int=253
+    fun checkPermission(){
+        if (Build.VERSION.SDK_INT>=23){
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),READIMAGE)
+                return
+            }
+        }
+        loadImage()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            READIMAGE->{
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    loadImage()
+                }else{
+                    Toast.makeText(this,"Cannot access your images.",Toast.LENGTH_LONG).show()
+                }
+            }
+            else->super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+
+    }
+    val PICK_IMAGE_CODE=123
+    fun loadImage(){
+        var intent=Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==PICK_IMAGE_CODE && data!=null && resultCode == RESULT_OK){
+            val selectedImage=data.data
+            val filePathColumn= arrayOf(MediaStore.Images.Media.DATA)
+            val cursor=contentResolver.query(selectedImage,filePathColumn,null,null,null)
+            cursor.moveToFirst()
+            val columnIndex=cursor.getColumnIndex(filePathColumn[0])
+            val picturePATH=cursor.getString(columnIndex)
+            cursor.close()
+            ivImagePerson.setImageBitmap(BitmapFactory.decodeFile(picturePATH))
+        }
     }
 
     fun buLogin(view: View){
